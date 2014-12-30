bd6897878b1072e9472854d93d9240b947e551a8
ibrodiallo12 <ibrodiallo@localhost.(none)>
2014-12-30 00:41:10 +0000

    quelque balise html de base et les KNOWS correspondant

--- assets/code/html.js
+++ assets/code/html.js
@@ -1,16 +1,14 @@
 $(function(){
 	//The separator in the ARRAY
 	var s = ",";
-	var code = "<strong>\n\tun gras word\n</strong>";
-	var know = "un simple strong";
-	code = code + s + "<?php echo \"Hello word\"; ?>";
-	know = know + s + "Affiche un Hello world en php";
-	code = code + s + "<head>\n\t<title>Hello world!</title></head>";
-	know = know + s + "Une balise head et title en html";
-	code = code + s + "<h1>Welcome</h1>";
-	know = know + s + "un gros title de niveau 1 en html";
-	code = code + s + "<script> alert(\"Hello!\"); </script>";
-	know = know + s + "un script JavaScript pour afficher une alert Hello";
+	var code = "<title>This is a title</title>";
+	var know = "A title in HTML";
+	code = code + s + "<img /> <img /> <img />";
+	know = know + s + "Print an Image in HTML";
+	code = code + s + "<!DOCTYPE html>\n<html>\n\t<head>\n\t<meta charset=\"utf-8\" />\n\t<title>Title</title>\n</head>\n<body>\n\n</body>\n</html>";
+	know = know + s + "Structuring basic html tag";
+	code = code + s + "<p>Good morning! Welcome to my website.</p>";
+	know = know + s + "A welcome paragraph";
 	$("#lang").text(code);
 	$("#know").text(know);
 	$("#separator").text(s);
