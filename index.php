ace5efa2e9261454211abe7c29e9e9d3ed60d367
ibrodiallo12 <ibrodiallo12@yahoo.fr>
2015-03-31 21:53:57 +0200

    ajout du carousel dans index.php

--- index.php
+++ index.php
@@ -22,6 +22,8 @@ if(isset($_COOKIE['lang'])){
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+     <!-- Custom styles for this template -->
+    <link href="assets/css/carousel.css" rel="stylesheet">
   </head>
 
   <body>
@@ -56,16 +58,77 @@ if(isset($_COOKIE['lang'])){
           <p>On MemryCode you do not just hit some letters but rather of the characters used in programming languages such as (} "<> @ # _ + _ ~` |? + + - % ^ & ()''/\ ! ....). And numbers you become faster with your keyboard because you hit all possible characters.</p>
         </div>
       </div>
+ 
+    </div><!-- /.container -->
+    
+	<!--Begin Carousel-->
+	<div style="margin-top: 30px; box-shadow: 0px 0px 5px black;">
+	<h4 class="text-muted" style="font-style: italic; text-align: center; padding-top: 10px;">........ How to use? Quickly .........</h4>
+    <!-- Carousel
+    ================================================== -->
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+      <!-- Indicators -->
+      <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+      </ol>
+      <div class="carousel-inner" role="listbox">
+        <div class="item active">
+          <img class="first-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="First slide">
+          <div class="container">
+            <div class="carousel-caption">
+              <img src="assets/icon/quickuse.png" />
+              <p>Type the code of the first block to the second block. Try to make fewer mistakes and try to reach the same number of second MIN of the right.</p>
+              <p><a class="btn btn-lg btn-primary" href="memrycode.php?lang=<?php echo $lang;?>" title="Try it!" role="button">Try by yourself</a></p>
+            </div>
+          </div>
+        </div>
+        <div class="item">
+          <img class="second-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Second slide">
+          <div class="container">
+            <div class="carousel-caption">
+              <img src="assets/icon/quicklang.png" />
+              <p>The blue button on top right allows you to choose the programming language you want to master.</p>
+              <p><a class="btn btn-lg btn-primary" href="memrycode.php?lang=<?php echo $lang;?>" title="Try it!" role="button">Make your choice</a></p>
+            </div>
+          </div>
+        </div>
+        <div class="item">
+          <img class="third-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Third slide">
+          <div class="container">
+            <div class="carousel-caption">
+              <img src="assets/icon/quickknow.png" />
+              <p>the KNOW section allows to understand the code you typing. Click the button view details for more information.</p>
+              <p><a class="btn btn-lg btn-primary" href="help.php" role="button">Know more</a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div><!-- /.carousel -->
+    <h1 class="text-muted" style="font-style: italic; text-align: center; padding-bottom: 60px;"><{>............Tap to the beat of your thinking ............<}></h1>
+    </div><!-- /.container -->
 
+    <script src="assets/js/jquery.js"></script>
+	 <script src="assets/js/bootstrap.min.js"></script>
+	 </div>
+	<!--End Carousel-->
+	
       <!-- Site footer -->
-      <footer class="footer">
-        <p style="margin-top: 50px;">&copy; 2014 MemryCode, Inc. &middot; <a href="help.php">Help</a> &middot; <a href="about.php">About</a></p>
+      
+      <footer class="footer" style="margin-bottom: -40px;">
+        <p style="text-align: center;">&copy; 2014 MemryCode, Inc. &middot; <a href="help.php">Help</a> &middot; <a href="about.php">About</a></p>
       </footer>
 
     </div> <!-- /container -->
-
-
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="assets/js/bootstrap.min.js"></script>
+ 	
   </body>
 </html>
